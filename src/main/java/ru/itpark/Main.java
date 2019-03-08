df3c55f5ed626a7da14a195fd87b6139eb4c57e4
Ramil Mubarakshin <20abdul10malik84>
2019-03-08 22:28:45 +0300

    Modification InitialServiceTest

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -2,7 +2,7 @@ package ru.itpark;
 
 public class Main {
     public static void main(String[] args) {
-        String first = "Hello"; // литеральная форма записи
-
+        String first = "Hello";
+        System.out.println(first);
     }
 }
--- src/test/java/ru/itpark/InitialServiceTest.java
+++ src/test/java/ru/itpark/InitialServiceTest.java
@@ -9,10 +9,37 @@ class InitialServiceTest {
     public void getInitialsTwoWordsName(){
         InitialService service = new InitialService();
         String name = "Ivan Petrov";
+        String expected = "IP";
 
         String actual = service.getInitials(name);
 
-        assertEquals("IP", actual);
+        assertEquals(expected, actual);
     }
+    public void getInitialsOneWordName() {
+        InitialService service = new InitialService();
+        String name = "Ivan";
+        String expected = "I";
 
+        String actual = service.getInitials(name);
+
+        assertEquals(expected, actual);
+    }
+    public void getInitialsThreeWordsName() {
+        InitialService service = new InitialService();
+        String name = "Huan Pedro Rodriges";
+        String expected = "HP";
+
+        String actual = service.getInitials(name);
+
+        assertEquals(expected, actual);
+    }
+    public void getInitialsWordsName() {
+        InitialService service = new InitialService();
+        String name = "ivan petrov";
+        String expected = "IP";
+
+        String actual = service.getInitials(name);
+
+        assertEquals(expected, actual);
+    }
 }
